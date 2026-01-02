df480e8831020a032161b9762c329058c0b47bd7
VladSteb237 <vladsteb@mail.ru>
2026-01-02 12:29:32 +0300

    fix(seed): generate recent createdAt dates for charts

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,10 +4,22 @@ const prisma = new PrismaClient();
 
 async function main() {
   const clerkId = "user_2w8utvKcLWQMIJBAGe6Z9Oa3cZg";
+  // const jobs = data.map((job) => {
+  //   return {
+  //     ...job,
+  //     clerkId,
+  //     createdAt: new Date(),
+  //   };
+  // });
+  const dayjs = require("dayjs");
+
   const jobs = data.map((job) => {
     return {
       ...job,
       clerkId,
+      createdAt: dayjs()
+        .subtract(Math.floor(Math.random() * 6), "month")
+        .toDate(),
     };
   });
   for (const job of jobs) {
