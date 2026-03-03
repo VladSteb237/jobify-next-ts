602a09e956b878e61f4a3733e6b947e6bfedb8b4
VladSteb237 <vladsteb@mail.ru>
2026-03-03 15:24:55 +0300

    change db connection to Neon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       </div>
       <div className="flex items-center gap-x-4">
         <ThemeToggle />
-        <UserButton afterSignOutUrl="/" />
+        <UserButton />
       </div>
     </nav>
   );
